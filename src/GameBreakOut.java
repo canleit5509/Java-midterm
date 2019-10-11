bc2cafefe50a5fc20d194794f2e6365ff373073f
Nghĩa Lê Văn <102170038@sv.dut.edu.vn>
2019-10-11 23:47:23 +0700

    Add class Ball
    Tao 1 ball va add class vector

--- src/GameBreakOut.java
+++ src/GameBreakOut.java
@@ -1,4 +1,7 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
@@ -9,6 +12,9 @@ public class GameBreakOut extends JFrame {
 	// Start Variable
 	int w = 700;
 	int h = 500;
+	Graphics g;
+	Ball b;
+	BufferedImage bufImg;
 	// End Variable
 	
 	
@@ -17,13 +23,113 @@ public class GameBreakOut extends JFrame {
 		this.setSize(w,h);
 		this.setDefaultCloseOperation(3);
 		
+		Random random = new Random();
+		b = new Ball (350,450,13,random.nextDouble()*5-2,1.5,this);
+		b.start();
 		
-		
-		
+		bufImg = new BufferedImage(w,h, BufferedImage.TYPE_3BYTE_BGR);
+		g = bufImg.getGraphics();
 		this.setVisible(true);
+		// Add cac su kien
+		
+		//
 	}
 	
 	public void paint(Graphics g1) {
+		g.setColor(Color.white);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		
+		// Set graphics cho ball
+		
+		g.setColor(Color.blue);
+		g.fillOval((int)(b.x-b.r), (int)(b.y-b.r), (int)(b.r*2), (int)(b.r*2));
+		g.drawOval((int)(b.x-b.r), (int)(b.y-b.r), (int)(b.r*2), (int)(b.r*2));
+		
+		
+		g1.drawImage(bufImg, 0, 0, this.getWidth(), this.getHeight(), null);
+		
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO: lam gi do
+		}
+		
+		repaint();
 	}
 }
+
+class Ball extends Thread {
+	double x, y, r, vx, vy;
+	GameBreakOut gb;
+	public Ball(double x, double y, double r, double vx, double vy, GameBreakOut gb) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.gb = gb;
+		this.vx = vx;
+		this.vy = vy;
+	}
+	
+	public void run() {
+		while(true) {
+			x = x +vx;
+			y = y + vy;
+			if(x - r <= 0 || x + r >= gb.w) {
+				vx = -vx;
+			}
+			if(y - r <=0 || y + r >= gb.h) {
+				vy = -vy;
+			}
+			
+			// Tang toc sau moi lan an bong;
+			
+			//
+			
+			// Delay mot time nho
+			try {
+				Thread.sleep(6);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+			}
+			//
+		}
+	}
+}
+
+class Panel extends Thread {
+	
+}
+
+// Class Vector
+class MyVector {
+	double x,y;
+	MyVector(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public MyVector Add(MyVector v) {
+		return new MyVector(this.x + v.x, this.y + v.y);
+	}
+
+	public MyVector Sub(MyVector v) {
+		return new MyVector(this.x - v.x, this.y - v.y);
+	}
+
+	public double dotP(MyVector v) {
+		return this.x*v.x + this.y*v.y;
+	}
+
+	public double length() {
+		return Math.sqrt(x*x+y*y);
+	}
+	
+	public MyVector Mult(double l) {
+		return new MyVector(x*l, y*l);
+	}
+	
+	public MyVector Norm() {
+		return Mult(1.0/this.length());
+	}
+}
+
